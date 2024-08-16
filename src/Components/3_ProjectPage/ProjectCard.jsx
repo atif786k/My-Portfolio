609fb26e52d2d247169ba47759f82baffca04839
Mohammad Atif <matif91201@gmail.com>
2024-08-16 16:57:14 +0530

    detail edit

--- src/Components/3_ProjectPage/ProjectCard.jsx
+++ src/Components/3_ProjectPage/ProjectCard.jsx
@@ -1,31 +1,7 @@
 import React, { useEffect } from "react";
 import "../3_ProjectPage/ProjectCard.css";
-import { gsap, Power4 } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const ProjectCard = (props) => {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    // gsap.from(".project-card1", {
-    //   scale: 0,
-    //   opacity: 0
-    // })
-    // gsap.to(".project-card1", {
-    //   scrollTrigger: {
-    //     trigger: ".project-card1",
-    //     start: "top 85%",
-    //     end: "bottom 40%",
-    //     scrub: true,
-    //     markers: true
-    //   },
-    //   scale: 1,
-    //   opacity: 1,
-    //   duration: 4,
-    //   stagger: 0.2,
-    //   ease: Power4.easeIn
-    // });
-  }, []);
   return (
     <>
       <div id="card1" className="project-card1">
